aca49a713cc513eae62d15a8dca0c4348c4f06f8
subuyugaby <subuyu.gaby@gmail.com>
2024-11-04 10:02:04 -0600

    feat(SD):Writing logs to SD

--- MonAir_Base_V3/MonAir_Base_V3.ino
+++ MonAir_Base_V3/MonAir_Base_V3.ino
@@ -9,22 +9,24 @@
 #include <OneWire.h>            // Para DS18B20
 #include <DallasTemperature.h>  // Para DS18B20
 #include "Adafruit_PM25AQI.h"   // Para sensor PMSA0031
+#include <SPI.h>
+#include <SD.h>
 
 // WiFi Credentials
 // Modify for assign your credentials
-const char *ssid = "galileo";
-const char *password = "";
+const char *ssid = "TIGO-C066";
+const char *password = "4D99ED806661";
 
 // MQTT Server
 const char *mqtt_server = "galiot.galileo.edu";
 // MQTT broker credentials
 const char *user = "monair";
 const char *passwd = "MONair2023";
-const char *clientID = "airmonq_00657097";  // Modify to assign to a database
+const char *clientID = "airmonq_006570AA";  // Modify to assign to a database
 
 //  Dashboard name
 // Modify to assign to a dashboard and database
-#define TEAM_NAME "airmon/00657097"
+#define TEAM_NAME "airmon/006570AA"
 #define NEOPIXEL_PIN 25
 #define NUMPIXELS 16
 
@@ -38,6 +40,7 @@ Bsec iaqSensor;
 String output, output2;
 
 //  Data variables
+int reconnectionCounter = 0;
 double temp, ds18, hume, pres = 0;
 double aqi, sAQI, AQIa = 0;
 double CO2e, VOCe, gas, rssi, pm10, pm25 = 0;
@@ -60,27 +63,68 @@ PM25_AQI_Data data;
 //  NeoPixel declaration for signals
 Adafruit_NeoPixel pixels(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
-/* This flag controls that the station is sending data in the stablished time 
-    - It is TRUE when the stablished time has passed, it allowes to send data
-    - FALSE: when data where sent
-*/
+
+#define CS_PIN 5  // Pin de selección del chip, ajusta según sea necesario
 
 void setup() {
   Serial.begin(115200);
-  
+
+  //TEMPORAL SOOLO PARA PROBAR RECONEXIÓN
+  pixels.setPixelColor(0, pixels.Color(218, 112, 214));
+  pixels.show();
+  delay(500);
+
   //WiFi setup
   setupWiFi();
-  
+
   //MQTT setup
   setupMQTT();
   timeClient.begin();
   timeClient.setTimeOffset(-21600);
+
+  SPI.begin(18, 19, 23, CS_PIN);
+  // Inicializa la tarjeta SD
+  if (!SD.begin(CS_PIN)) {
+    Serial.println("No se pudo inicializar la tarjeta SD.");
+  }
+  Serial.println("Tarjeta SD inicializada correctamente.");
+
+  // Manejo del primer archivo
+  if (!SD.exists("/logs.txt")) {
+    File archivo1 = SD.open("/logs.txt", FILE_WRITE);
+    if (archivo1) {
+      archivo1.println("Inicialización de logs");
+      archivo1.close();
+      Serial.println("Archivo logs.txt creado y datos escritos.");
+    } else {
+      Serial.println("Error al crear logs.txt.");
+    }
+  } else {
+    Serial.println("El archivo logs.txt ya existe. Se continuará escribiendo en él.");
+  }
+
+  // Manejo del segundo archivo
+  if (!SD.exists("/data.txt")) {
+    File archivo2 = SD.open("/data.txt", FILE_WRITE);
+    if (archivo2) {
+      archivo2.println("temp,hume,pres,aqi,sAQI,AQIa,gas,CO2e,VOCe,rssi,pm25,pm10,ds18");
+      archivo2.close();
+      Serial.println("Archivo data.txt creado y datos escritos.");
+    } else {
+      Serial.println("Error al crear data.txt.");
+    }
+  } else {
+    Serial.println("El archivo data.txt ya existe. Se continuará escribiendo en él.");
+  }
+
+
 
   //BME680 Setup
   Wire.begin(21, 22);
   iaqSensor.begin(0x77, Wire);
   output = "BSEC library version: " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." + String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
   Serial.println(output);
+  appendFile(SD, "/logs.txt", output.c_str());
   bsec_virtual_sensor_t sensorList[10] = {
     BSEC_OUTPUT_RAW_TEMPERATURE,
     BSEC_OUTPUT_RAW_PRESSURE,
@@ -99,6 +143,7 @@ void setup() {
   // Print the header
   output = "************** Data Variables ************** \n\t-Timestamp [ms] \n\t-raw temperature [°C] \n\t-pressure [hPa] \n\t-raw relative humidity [%] \n\t-gas [Ohm] \n\t-IAQ \n\t-IAQ accuracy\n\t-temperature [°C] \n\t-relative humidity [%]\n\t-Static IAQ \n\t-CO2 equivalent \n\t-breath VOC equivalent ";
   Serial.println(output);
+  appendFile(SD, "/logs.txt", output.c_str());
   delay(3000);
 
   /***** PMSA0031 sensor *********/
@@ -110,47 +155,62 @@ void setup() {
       delay(10);
   } else {
     Serial.println("PM25 found!");
+    appendFile(SD, "/logs.txt", "PM25 found!");
   }
 }
 
 void loop() {
   //checking WiFi conection
   if (WiFi.status() != WL_CONNECTED) {
-    pixels.setPixelColor(0, pixels.Color(255, 0, 0));
-    pixels.show();
-    delay(500);
+    appendFile(SD, "/logs.txt", "LOOP: WiFi disconnected ");
     setupWiFi();
   }
- // if (!mqtt_client.connected()) {
-  mqtt_client.loop(); //This should be called regularly to allow the client to process incoming messages and maintain its connection to the server.
-
+  // if (!mqtt_client.connected()) {
+  mqtt_client.loop();  //This should be called regularly to allow the client to process incoming messages and maintain its connection to the server.
   if (mqtt_client.state() != 0) {
     pixels.setPixelColor(0, pixels.Color(255, 255, 0));
     pixels.show();
     delay(500);
-    Serial.println("reconnect LOOP");
+    appendFile(SD, "/logs.txt", "LOOP: MQTT disconnected ");
     reconnect();
   }
 
   timeClient.update();
-  if (timeClient.getSeconds() == 15) {
-      if ((WiFi.status() != WL_CONNECTED) && (!mqtt_client.connect(clientID, user, passwd))) {
-        String str69 = "Online"; // Se cambio revisar en Node-Red
-        str69.toCharArray(msg, 50);
-        mqtt_client.publish(getTopic("Online"), msg);
-      }
+  if (timeClient.getSeconds() % 15 == 0) {
+    if ((WiFi.status() == WL_CONNECTED) && (mqtt_client.connect(clientID, user, passwd))) {
+      String str69 = "Online";  // Se cambio revisar en Node-Red
+      str69.toCharArray(msg, 50);
+      mqtt_client.publish(getTopic("Online"), msg);
     }
+  }
 
   //Checks posting_flag and post the data
-  int posting_time = 10;
+  int posting_time = 2;
   if ((timeClient.getMinutes() % posting_time == 0) && (timeClient.getSeconds() == 00)) {
     Serial.print(String(posting_time) + " minutos publicando");
     postData();
-    Serial.print("Datos publicados en MQTT Server: ");
-    pixelSignals(0, 0, 255, 1000);
+    Serial.print("Datos publicados en MQTT Server");
+    appendFile(SD, "/logs.txt", "Datos publicados en MQTT Server");
   }
 
-  preHeatSensor(); 
+  preHeatSensor();
+}
+
+
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Appending to file: %s\n", path);
+  
+  File file = fs.open(path, FILE_APPEND);
+  if (!file) {
+    Serial.println("Error al abrir el archivo para agregar datos.");
+    return;
+  }
+  if (file.print(message) && file.println()) { // Agregar salto de línea
+    Serial.println("Mensaje agregado.");
+  } else {
+    Serial.println("Fallo al agregar el mensaje.");
+  }
+  file.close();
 }
 
 
@@ -176,9 +236,16 @@ void setupWiFi() {
 
   //Waiting for connection
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print("WiFi no conectado");
+    Serial.print(".");
     pixels.setPixelColor(0, pixels.Color(255, 0, 0));
     pixels.show();
+    appendFile(SD, "/logs.txt", "SETUP WIFI: Intento de Reconexión");
+    reconnectionCounter += 1;
+    delay(5000);
+    if (reconnectionCounter > 250) {
+      appendFile(SD, "/logs.txt", "SETUP WIFI: Reiniciando ESP32");
+      ESP.restart();
+    }
   }
   Serial.println("");
   Serial.println("************** Connection information **************");
@@ -187,6 +254,7 @@ void setupWiFi() {
   Serial.println(WiFi.localIP());
   Serial.print("ESP Board MAC Address:  ");
   Serial.println(WiFi.macAddress());
+  appendFile(SD, "/logs.txt", "SETUP WIFI: Conectado");
   //Indicates connection
   pixelSignals(0, 255, 0, 500);
   pixelSignals(0, 255, 0, 500);
@@ -208,19 +276,30 @@ void reconnect() {
   // Loop until we're reconnected
   int cont = 1;
   while (!mqtt_client.connected()) {
-    setupWiFi();
+    if (WiFi.status() != WL_CONNECTED) {
+      setupWiFi();
+    }
+    pixels.setPixelColor(0, pixels.Color(255, 255, 0));
+    pixels.show();
+    delay(5000);
+    appendFile(SD, "/logs.txt", "SETUP MQTT: Intento de Reconexión");
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
 
     if (mqtt_client.connect(clientID, user, passwd)) {
       //mqtt_client.subscribe(getTopic("rgb"));
+      appendFile(SD, "/logs.txt", "SETUP MQTT: Conectado");
       Serial.println("connected");
       pixelSignals(0, 255, 0, 500);
       pixelSignals(0, 255, 0, 500);
       pixelSignals(0, 255, 0, 500);
 
     } else {
-
+      reconnectionCounter += 1;
+      if (reconnectionCounter > 250) {
+        appendFile(SD, "/logs.txt", "MQTT reconnection: Reiniciando ESP32");
+        ESP.restart();
+      }
       Serial.print("failed, rc=");
       Serial.print(mqtt_client.state());
       Serial.println(" try again in 5 seconds");
@@ -251,20 +330,22 @@ void callback(char *topic, byte *payload, unsigned int length) {
 void setupMQTT() {
   pixels.setPixelColor(0, pixels.Color(255, 255, 0));
   pixels.show();
+  delay(5000);
   mqtt_client.setServer(mqtt_server, 1883);
   mqtt_client.setCallback(callback);
+  reconnect();
 }
 
 //This function recolects the data from the sensors and post it
 void postData() {
-  mqtt_client.setServer(mqtt_server, 1883);
-  mqtt_client.setCallback(callback);
-  delay(100);
   String missing = "Datos no enviados: \n";
   String sent = "Datos enviados: \n";
-  if (mqtt_client.connect(clientID, user, passwd)) {
+  Serial.print("DESCONECTAR AQUÍ");
+  if (WiFi.status() == WL_CONNECTED && mqtt_client.connect(clientID, user, passwd)) {
     Serial.println("");
     Serial.println("*** Cliente conectado a MQTT Server");
+
+    
     //************ Posting Temperature************
     temp = iaqSensor.temperature;
     //Serial.println("Temperatura : " + String(temp));
@@ -349,7 +430,8 @@ void postData() {
     (mqtt_client.publish(getTopic("ds18"), msg)) ? sent += "\t- DS18 enviado\n" : missing += "DS18\n";
 
     Serial.println(sent);
-
+    pixelSignals(0, 0, 255, 1000);
+    appendFile(SD, "/logs.txt", "POST DATA: Datos enviados");
     if (missing != "Datos no sent: \n") {
       Serial.println(missing);
       Serial.println("Estado de la conexión MQTT Server: ");
@@ -361,13 +443,14 @@ void postData() {
       (mqtt_client.publish(getTopic("info"), msg)) ? Serial.println("Log enviado.") : Serial.println("Fallo en envio de log");
     }
   } else {
+    mqtt_client.loop();
+    appendFile(SD, "/logs.txt", "POST DATA: MQTT o WIFI desconectado");
     Serial.println("Cliente NO conectado a MQTT Server");
     Serial.print("Estado del error de conexión: ");
     Serial.println(mqtt_client.state());
     setupWiFi();
     Serial.println("Reconnect postdata else ");
     reconnect();
-    
   }
 }
 
